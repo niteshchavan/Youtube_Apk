a8688576e3e66bc6c8e0af6cecc0f85b9c0267db
Nitesh Chavan <76774483+niteshchavan@users.noreply.github.com>
2023-05-13 22:43:01 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,38 +1,103 @@
-package com.example.myapp;
+package com.nitesh.myapplication;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v4.media.session.MediaControllerCompat;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
 import android.view.KeyEvent;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements View.OnKeyListener {
+
+public class MainActivity extends AppCompatActivity {
+    MediaSessionCompat mediaSession;
+    PlaybackStateCompat.Builder stateBuilder;
+
+    private static final String LOG_TAG = "MainActivity";
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        // Set the key listener on the root view of the activity
-        View rootView = getWindow().getDecorView().getRootView();
-        rootView.setOnKeyListener(this);
+
+        // Create a MediaSessionCompat
+        mediaSession = new MediaSessionCompat(this, LOG_TAG);
+
+        // Enable callbacks from MediaButtons and TransportControls
+        mediaSession.setFlags(
+                MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS |
+                        MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS);
+
+        // Do not let MediaButtons restart the player when the app is not visible
+        mediaSession.setMediaButtonReceiver(null);
+
+        // Set an initial PlaybackState with ACTION_PLAY, so media buttons can start the player
+        stateBuilder = new PlaybackStateCompat.Builder()
+                .setActions(
+                        PlaybackStateCompat.ACTION_PLAY |
+                                PlaybackStateCompat.ACTION_PLAY_PAUSE);
+        mediaSession.setPlaybackState(stateBuilder.build());
+
+        // MySessionCallback has methods that handle callbacks from a media controller
+        mediaSession.setCallback(new MySessionCallback());
+
+        // Create a MediaControllerCompat
+        MediaControllerCompat mediaController =
+                new MediaControllerCompat(this, mediaSession);
+
+        MediaControllerCompat.setMediaController(this, mediaController);
+
+
     }
-
     @Override
-    public boolean onKey(View v, int keyCode, KeyEvent event) {
-        Log.d("eventss", String.valueOf(event.getAction()));
+    public boolean dispatchKeyEvent(KeyEvent event) {
+
         if (event.getAction() == KeyEvent.ACTION_DOWN) {
-            // Key down event
-            switch (keyCode) {
-                case KeyEvent.KEYCODE_VOLUME_UP:
-                    // Handle volume up key press
+            switch (event.getKeyCode()) {
+                case KeyEvent.KEYCODE_HEADSETHOOK:
+                    // Handle headset button press event
+                    if (mediaSession != null) {
+                        MediaControllerCompat mediaController = mediaSession.getController();
+                        PlaybackStateCompat playbackState = mediaController.getPlaybackState();
+                        if (playbackState != null) {
+                            switch (playbackState.getState()) {
+                                case PlaybackStateCompat.STATE_PLAYING:
+                                    mediaController.getTransportControls().pause();
+                                    break;
+                                case PlaybackStateCompat.STATE_PAUSED:
+                                case PlaybackStateCompat.STATE_STOPPED:
+                                    mediaController.getTransportControls().play();
+                                    break;
+                            }
+                        }
+                    }
                     return true;
-                case KeyEvent.KEYCODE_VOLUME_DOWN:
-                    // Handle volume down key press
-                    return true;
-                // Add more key codes and corresponding actions as needed
             }
         }
-        return false;
+        return super.dispatchKeyEvent(event);
+    }
+
+    private static class MySessionCallback extends MediaSessionCompat.Callback {
+
+        @Override
+        public void onPlay() {
+            // Implement your logic for handling the play command
+        }
+
+        @Override
+        public void onPause() {
+            // Implement your logic for handling the pause command
+
+        }
+
+        @Override
+        public void onStop() {
+            // Implement your logic for handling the stop command
+        }
+
+        // Add more overridden methods as per your requirements
+
     }
 }
